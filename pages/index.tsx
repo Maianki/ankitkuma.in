169bd208fea51f95d4d89ac560608f9126ac98bd
Ankit Kumain <kumankit10@gmail.com>
2022-12-01 11:53:32 +0530

    tailwind complete setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
-import CatCard from '../components/cards/cat/CatCard';
-import { mockCatCardProps } from '../components/cards/cat/CatCard.mocks';
-import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
-import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
+    <section className="bg-gradient-to-r from-cyan-500 to-blue-500 backdrop-blur">
       <h1>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
-      <CatCard {...mockCatCardProps.base} />
     </section>
   );
 };
@@ -18,10 +13,5 @@ const Home: NextPageWithLayout = () => {
 export default Home;
 
 Home.getLayout = (page) => {
-  return (
-    <PrimaryLayout>
-      <SidebarLayout />
-      {page}
-    </PrimaryLayout>
-  );
+  return <div>Configured</div>;
 };
